559d9b06079decb8d70df01a181f1ca96370ef57
Marma__duk <160778212+Marmaduk0x@users.noreply.github.com>
2024-07-28 13:03:23 +0200

    added function

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,12 @@
 const Migrations = artifacts.require("Migrations");
+const CryptoKittiesCore = artifacts.require("CryptoKittiesCore");
+const CryptoKittiesSales = artifacts.require("CryptoKittiesSales");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Migrations)
+    .then(() => deployer.deploy(CryptoKittiesCore))
+    .then(() => CryptoKittiesCore.deployed())
+    .then((coreInstance) => {
+        return deployer.deploy(CryptoKittiesSales, coreInstance.address);
+    });
 };
