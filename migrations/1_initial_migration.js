589840ba9e3b1cc354c71310f7ea129289b3dc0d
Marma__duk <160778212+Marmaduk0x@users.noreply.github.com>
2024-07-29 21:33:05 +0200

    implemented new feature

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,6 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Import the base ERC721 contracts from OpenZeppelin.
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract CryptoKittiesBreeding {
+    address public cryptoKittiesCoreAddress;
+
+    constructor(address _cryptoKittiesCoreAddress) {
+        cryptoKittiesCoreAddress = _cryptoKittiesCoreAddress;
+    }
+
+    // Placeholder function for breeding kitties
+    function breed(uint256 parent1, uint256 parent2) external {
+        // Implement your breeding logic here.
+        // For example, you could create a new kitty with attributes derived from the parent IDs.
+    }
+}
+```
+```javascript
 const Migrations = artifacts.require("Migrations");
 const CryptoKittiesCore = artifacts.require("CryptoKittiesCore");
 const CryptoKittiesSales = artifacts.require("CryptoKittiesSales");
+const CryptoKittiesBreeding = artifacts.require("CryptoKittiesBreeding"); // Include your new contract
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations)
@@ -8,5 +30,10 @@ module.exports = function(deployer) {
     .then(() => CryptoKittiesCore.deployed())
     .then((coreInstance) => {
         return deployer.deploy(CryptoKittiesSales, coreInstance.address);
+    })
+    .then(() => CryptoKittiesCore.deployed())
+    .then((coreInstance) => {
+        // Deploy the Breeding contract, passing the address of CryptoKittiesCore
+        return deployer.deploy(CryptoKittiesBreeding, coreInstance.address);
     });
 };
